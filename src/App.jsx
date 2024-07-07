e9d67cc00af55b4d501f7d71695ada22b448efa5
MI-Israr <m.israr1228@gamil.com>
2024-07-07 21:14:22 +0500

    modified files

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="flex justify-center items-center h-screen w-screen bg-gray-600">
-      <h1 className="text-3xl text-white font-bold ">Hello world...</h1>
+    <div>
+      <h1>Hello world...</h1>
     </div>
   );
 };
