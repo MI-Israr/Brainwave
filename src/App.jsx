8de537ebf030973de620302d2e0b98a0a6e765f3
MI-Israr <m.israr1228@gamil.com>
2024-07-07 20:43:43 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen bg-gray-600">
+      <h1 className="text-3xl text-white font-bold ">Hello world...</h1>
+    </div>
+  );
+};
+
+export default App;
