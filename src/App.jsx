6264fb2968a64ad7b98ab7f89a1ebda5e0bfe839
MI-Israr <m.israr1228@gamil.com>
2024-07-10 20:33:22 +0500

    Hero section completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
 import Button from "./components/Button.jsx";
 import Header from "./components/Header.jsx";
+import Hero from "./components/Hero.jsx";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,7 +1,7 @@
 import SectionSvg from "../assets/svg/SectionSvg";
 
 const Section = ({
-  classes,
+  className,
   id,
   crosses,
   crossesOffset,
@@ -10,12 +10,12 @@ const Section = ({
 }) => {
   return (
     <div
-      className={`relative ${
-        customPaddings ||
-        `py-10 lg:py-16 xl:20 ${crosses ? "lg:py-32 xl:py-40" : ""} ${
-          classes || ""
-        }`
-      }`}
+      id={id}
+      className={`relative 
+    ${
+      customPaddings ||
+      `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+    } ${className || ""}`}
     >
       {children}
       <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10" />
