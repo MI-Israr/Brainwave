229813a6bdb0f1925bdfdcd358bfe44ce573a412
MI-Israr <m.israr1228@gamil.com>
2024-07-11 23:11:36 +0500

    servies started

---
+++ src/components/Services.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Section from "./Section";
+import Heading from "./Heading.jsx";
+import { service1 } from "../assets/index.js";
+
+const Services = () => {
+  return (
+    <Section id="how-to-use">
+      <div className="container">
+        <Heading
+          title="Generative AI made for creators."
+          text="Brainwave unlocks the potential of AI-powered applications."
+        />
+        <div className="relative">
+          <div className="relative z-10 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:[h-46rem]">
+            <div>
+              <img
+                className="w-full h-full object-cover md:object-right"
+                width={800}
+                height={730}
+                src={service1}
+                alt="Smartest AI"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default Services;
