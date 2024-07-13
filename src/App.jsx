03e405fa1879d0a6b99c550bafe96ff5895787a3
MI-Israr <m.israr1228@gamil.com>
2024-07-13 23:01:51 +0500

    Roadmap completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Benefits from "./components/Benefits.jsx";
 import Collaboration from "./components/Collaboration.jsx";
 import Services from "./components/Services.jsx";
 import Pricing from "./components/Pricing.jsx";
+import Roadmap from "./components/Roadmap.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
